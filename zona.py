d23b5e7daa07049c0bfd3fa7b8a622369092bb64
MiltonSeit <milton0330@gmail.com>
2017-04-08 18:25:01 -0300

    Agregado de Obra Social

--- zona.py
+++ zona.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-#Definimos la clase Docente
+#Definimos la clase Zona
 class Zona(object):
     __cod_Zona = None
     __descripcion_Zona = None
-    __porcentaje_Zona
+    __porcentaje_Zona = None
 
     """Instanciador"""
     def __init__(self, cod_Zona, descripcion_Zona, porcentaje_Zona):
         self.__cod_Zona = cod_Zona
         self.__descripcion_Zona = descripcion_Zona
-        self.__porcentaje_Zona
+        self.__porcentaje_Zona = porcentaje_Zona
 
     def getCod_Zona(self):
         return self.__cod_Zona
@@ -27,7 +26,7 @@ class Zona(object):
         self.__cod_Zona = cod_Zona
 
     def setDescripcion_Zona(self,descripcion_Zona):
-        self.__descripcion_Zona = des
+        self.__descripcion_Zona = descripcion_Zona
 
     def setPorcentaje_Zona(self, porcentaje_Zona):
         self.__porcentaje_Zona = porcentaje_Zona
@@ -35,5 +34,3 @@ class Zona(object):
     cod_zona    = property(fget = getCod_Zona, fset = setCod_Zona)
     descripcion_Zona = property(fget = getDescripcion_Zona, fset = setDescripcion_Zona)
     porcentaje_Zona = property(fget = getPorcentaje_Zona, fset = setPorcentaje_Zona)
-
-    
