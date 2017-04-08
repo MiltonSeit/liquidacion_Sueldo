2fc1f6c364381bc73a83270afd5277af43324f4f
MiltonSeit <milton0330@gmail.com>
2017-04-08 18:50:45 -0300

    Comentarios en zona.py

--- zona.py
+++ zona.py
@@ -7,27 +7,67 @@ class Zona(object):
     __descripcion_Zona = None
     __porcentaje_Zona = None
 
-    """Instanciador"""
+    """
+    * Constructor
+    * @param cod_Zonal,descripcion_Zona, porcentaje_Zona
+    * @return no devuelve nada
+    """
     def __init__(self, cod_Zona, descripcion_Zona, porcentaje_Zona):
         self.__cod_Zona = cod_Zona
         self.__descripcion_Zona = descripcion_Zona
         self.__porcentaje_Zona = porcentaje_Zona
 
+    """
+     * Getter cod_Zona.
+     * @param Ninguno.
+     * @return devuelve el codigo de la Zona
+     */
+    """
     def getCod_Zona(self):
         return self.__cod_Zona
 
+    """
+     * Getter descripcion_Zona.
+     * @param Ninguno.
+     * @return devuelve la descripcion de la zona(favorable, desfavorable).
+     */
+    """
     def getDescripcion_Zona(self):
         return self.__descripcion_Zona
 
+    """
+     * Getter porcentaje_Zona.
+     * @param Ninguno.
+     * @return devuelve el porcentaje de la zona de acuerdo a la zona.
+     */
+    """
     def getPorcentaje_Zona(self):
         return self.__porcentaje_Zona
 
+    """
+     * Setter cod_Zona
+     * @param cod_Zona.
+     * @return No devuelve nada.
+     */
+     """
     def setCod_Zona(self, cod_Zona):
         self.__cod_Zona = cod_Zona
 
+    """
+     * Setter descripcion_Zona
+     * @param descripcion_Zona.
+     * @return No devuelve nada.
+     */
+     """
     def setDescripcion_Zona(self,descripcion_Zona):
         self.__descripcion_Zona = descripcion_Zona
 
+    """
+     * Setter porcentaje_Zona
+     * @param porcentaje_Zona.
+     * @return No devuelve nada.
+     */
+     """
     def setPorcentaje_Zona(self, porcentaje_Zona):
         self.__porcentaje_Zona = porcentaje_Zona
 
