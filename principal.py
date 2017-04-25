83914e6f4e2715b37e07eb5188e35f90fbd8bb0b
MiltonSeit <milton0330@gmail.com>
2017-04-24 21:57:38 -0300

    Documentacion de la ventana principal

--- principal.py
+++ principal.py
@@ -24,32 +24,48 @@ def antiguedad():
     else:
         return anios
 
+"""Función obra social
+* @param no recibe ningún parámetro
+* @return devuelve el código de la obra social
+"""
 def obraS():
     valor={'Asimira': 1, 'Medisur': 2, 'Sps Salud': 3,'Osecac': 4,'Ioscor': 5}
     opc=valor[respo1.get()]
     return opc
 
+"""Función escuela
+* @param no recibe ningún parámetro
+* @return devuelve el numero de la escuela
+"""
 def escu():
     valor={'Isabel Estela Vera': 1, 'Agop Seferian': 2,'Bicentenario': 3,'Angel Acuna': 12,'Bernardino Rivadia': 666}
     opc2=valor[respo2.get()]
     return opc2
 
+"""Función cargos
+* @param no recibe ningún parámetro
+* @return devuelve el código del cargo que tiene el Docente
+"""
 def cargos():
     valor={'Bibliotecarios': 1, 'Jefe Preceptores Primaria': 3, 'Jefe Coordinador': 4,'Maestro de Grado': 5,'Maestro de Grado Esc Hogar': 6,'Maestro Especial Esc Adulto': 7,'Rector Superior': 8,'Supervisor': 9}
     opc3=valor[respo3.get()]
     return opc3
 
-#Funciones de los botones
+"""Función agregar_Docente
+* @param no recibe ningún parámetro
+* @return da de alta al docente y asigna el cargo
+"""
 def agregar_Docente():
-
+    #Instancia el Docente y da de alta.
     docente = Docente(entra_dni.get(), antiguedad(), obraS(), entra_nom.get(), entra_ape.get(), entra_dire.get(), entra_tel.get(), entra_fecha.get())
     docente.altaDocente()
 
+    #Instancia el cargo y lo da de alta.
     asigna = Asigna(entra_dni.get(), cargos(), escu())
     asigna.asignarCargo()
 
 
-#ventana Principal
+#Crea la ventana Principal
 ventana = Tk()
 ventana.title("Principal")
 ventana.geometry("1000x500+200+200")
@@ -71,21 +87,21 @@ lblImagen= Label(centro, image= imagenT).place(x=0, y=0)
 
 #funciones
 
+"""Función Alta_Docente
+* @param no recibe ningún parámetro
+* @return devuelve abre una nueva ventana para agregar los datos y dar de alta al Docente
+"""
 def Alta_Docente():
-
 
 	punto= Toplevel()
 	punto.title("Alta-Docente")
 	punto.geometry("1000x500+200+200")
-
 
 	medio=Frame(punto, width=1000, height=650)
 	medio.pack(side=BOTTOM)
 
 	imagen1=PhotoImage(file="imagenes/turq2.png")
 	lblImagen= Label(medio, image= imagen1).place(x=0, y=0)
-
-
 
 	#etiquetas
 	lblNom= Label(medio,text="Nombre", font=("Time", 15)).place(x=50, y=30)
@@ -94,9 +110,6 @@ def Alta_Docente():
 	lblDirecc= Label(medio,text="Direccion", font=("Time", 15)).place(x=500, y=130)
 	lblTel= Label(medio,text="Telefono", font=("Time", 15)).place(x=50, y=230)
 	lblFech= Label(medio,text="Fecha-Ingreso", font=("Time", 15)).place(x=500, y=230)
-	#lblObra= Label(medio,text="Obra Social", font=("Time", 15)).place(x=50, y=330)
-
-
 
 	global entra_nom
 	global entra_dni
@@ -132,35 +145,19 @@ def Alta_Docente():
 	opciones = ['Seleccione obra Social','Asimira', 'Medisur', 'Sps Salud','Osecac','Ioscor']
 	entra01 = OptionMenu (medio, respo1,*opciones, command= obraS).place(x=200,y=300)
 	respo1.set(opciones[0])
-	#print (respo1.get())
-
-
-
 
 	respo2=StringVar(medio)
 	opciones = ['Seleccione Escuela','Isabel Estela Vera', 'Agop Seferian','Bicentenario','Angel Acuna','Bernardino Rivadia']
 	entra02 = OptionMenu (medio, respo2,*opciones,command= escu).place(x=400,y=300)
 	respo2.set(opciones[0])
-	#print (respo2.get())
-
-
 
 	respo3=StringVar(medio)
 	opciones = ['Seleccione Cargo','Bibliotecarios', 'Jefe Preceptores Primaria', 'Jefe Coordinador','Maestro de Grado','Maestro de Grado Esc Hogar','Maestro Especial Esc Adulto','Rector Superior','Supervisor']
 	entra03 = OptionMenu (medio, respo3,*opciones,command= cargos).place(x=600,y=300)
 	respo3.set(opciones[0])
-	#print (respo3.get())
-
-	#entra_obra = StringVar()
-
-	#obra=Entry(medio, textvariable=entra_obra, font=("Arial", 13)).place(x=200, y= 330)
-
-
 
 	#Boton Agregar persona
 	BotonAgrega = Button(medio, text="Guardar", font=("Arial", 14), relief=RIDGE , activebackground ="brown", width=19, command= agregar_Docente).place(x=230, y=400)
-
-
 
 	#Boton salir
 	BotonSalir = Button(medio, text="Salir", font=("Arial", 14), relief=RIDGE, activebackground ="brown", command = punto.destroy, width=19).place(x=500,y=400)
@@ -175,6 +172,5 @@ btonLista=Button(centro, text="Listar", font=("Time", 15), width=12).place(x=700
 btonCalcula=Button(centro, text="Calcular Sueldo", font=("Time", 15), width=12).place(x=150, y=190)
 btonVisualiza=Button(centro, text="Previsualizar", font=("Time", 15), width=10).place(x=425, y=190)
 btonSalir= Button(centro, text="Salir", font=("Time", 15), width=10, command=ventana.destroy).place(x=800, y=350)
-
 
 ventana.mainloop()
